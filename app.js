9b9c144b5951cea3811d48d5aeaf2abc8f293d29
Lewis Lockhart <lewis00001@gmail.com>
2019-11-19 12:16:21 -0700

    bumped js into own file to make Chrome happy

---
+++ app.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+    let numDots = 36;
+    for (let i = 0; i < numDots; i++) {
+        $(".content").append("<div class='circ off'></div>");
+    }
+
+    $('.circ').on('click', function (e) {
+
+        if ($(this).hasClass("b")) {
+            $(this).toggleClass("b g");
+        } else if ($(this).hasClass("g")) {
+            $(this).toggleClass("g r");
+        } else if ($(this).hasClass("r")) {
+            $(this).toggleClass("r off");
+        } else if ($(this).hasClass("off")) {
+            $(this).toggleClass("off b");
+        }
+    });
+});
