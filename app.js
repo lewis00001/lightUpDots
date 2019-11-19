57aadc5760a40e70ef3bf137d90f401e4c172e01
Lewis Lockhart <lewis00001@gmail.com>
2019-11-19 15:51:27 -0700

    added clear lights button, function notes in the js

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 $(document).ready(function () {
-
+    // reloads the html page when the span is clicked
+    $('span').on('click', function () {
+        location.reload();
+    });
+    // generates all the dots on the screen
     let numDots = 256;
     for (let i = 0; i < numDots; i++) {
         $(".content").append("<div class='circ off'></div>");
     }
-
+    // cycles though colors based on currently displayed color
     $('.circ').on('click', function (e) {
 
         if ($(this).hasClass("b")) {
