6459ec8dfe96130e7c9784d0aca892df61cf50b8
Lewis Lockhart <lewis00001@gmail.com>
2020-05-26 19:29:29 -0600

    added glow toggle, css, js, html

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
 $(document).ready(function () {
+
+    // variables
+    let numDots = 208;
+    let activeColor = "glow-pink";
+    let dotShape = "circ";
+    let glow = true;
+
     // reloads the html page
     $('#clear').on('click', function () {
         location.reload();
     });
     // generates all the dots on the screen
-    let numDots = 208;
     for (let i = 0; i < numDots; i++) {
-        $(".dot-output").append("<div class='dot circ set-off'></div>");
+        $(".dot-output").append("<div class='dot circ glow-off'></div>");
     }
-    // active color selection
-    let activeColor = "set-pink";
-    let activeColorSelected = "pink";
-    // records color selection
-    $('.color-options').on('click', function () {
-        activeColor = $(this).attr('value');
-        console.log(activeColorSelected);
-        activeColorSelected = activeColor.replace("set-","");
-        $('#active-color').removeClass();
-        $('#active-color').addClass(activeColorSelected);
-    });
 
-    let dotShape = "circ";
+    // records color selection - checks for glow
+    $('.color-options').on('click', function () {
+        if (glow) {
+            activeColor = $(this).attr('value');
+            $('#active-color').removeClass();
+            $('#active-color').addClass(activeColor);
+        } else {
+            activeColor = $(this).attr('value').replace("glow-", "");
+            $('#active-color').removeClass();
+            $('#active-color').addClass(activeColor);
+        }
+    });
 
     // adds currently selected color to circ
     $('.dot').on('click', function (e) {
@@ -29,20 +35,40 @@ $(document).ready(function () {
     });
 
     // sets shape of dot in the grid
-    $(".toggle").on('click', function (e) {
+    $('.toggle-shape').on('click', function (e) {
         let clicked = $(this).hasClass('t-circle');
         // change from square to circle
-        if(clicked) {
+        if (clicked) {
             $('.t-circle').addClass('toggle-active');
             $('.t-square').removeClass('toggle-active');
             $('.dot').removeClass('sqr').addClass('circ');
             dotShape = "circ";
-        // change from circle to square 
+            // change from circle to square 
         } else {
             $('.t-circle').removeClass('toggle-active');
             $('.t-square').addClass('toggle-active');
             $('.dot').removeClass('circ').addClass('sqr');
             dotShape = "sqr";
+        }
+    });
+
+    // sets glow boolean
+    $('.toggle-glow').on('click', function (e) {
+        let clicked = $(this).hasClass('t-glow-on');
+        if (clicked) {
+            $('.t-glow-on').addClass('toggle-active');
+            $('.t-glow-off').removeClass('toggle-active');
+            activeColor = "glow-" + activeColor;
+            glow = true;
+            $('#active-color').removeClass();
+            $('#active-color').addClass(activeColor);
+        } else {
+            $('.t-glow-off').addClass('toggle-active');
+            $('.t-glow-on').removeClass('toggle-active');
+            activeColor = activeColor.split('-').pop();
+            glow = false;
+            $('#active-color').removeClass();
+            $('#active-color').addClass(activeColor);
         }
     });
 });
